00ea6aa4651df67148f0400c260bb63b93db1314
lucascawai <lucascawai@gmail.com>
2025-04-01 02:16:00 -0300

    Clean unused file

--- case-opovo/app/Http/Controllers/JournalistController.php
+++ case-opovo/app/Http/Controllers/JournalistController.php
@@ -6,28 +6,5 @@ use Illuminate\Http\Request;
 
 class JournalistController extends Controller
 {
-    public function register(Request $request)
-    {
-        // Validação dos dados
-        $validator = Validator::make($request->all(), [
-            'nome' => 'required|string|max:255',
-            'sobrenome' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:journalists',
-            'senha' => 'required|string|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        // Criar o jornalista
-        $journalist = Journalist::create([
-            'nome' => $request->nome,
-            'sobrenome' => $request->sobrenome,
-            'email' => $request->email,
-            'senha' => Hash::make($request->senha),
-        ]);
-
-        return response()->json(['message' => 'Jornalista registrado com sucesso!', 'journalist' => $journalist], 201);
-    }
+    //
 }
