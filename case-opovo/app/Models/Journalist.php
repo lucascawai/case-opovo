69507b57e0cf9b176c3c8304bb5af817542ee66b
lucascawai <lucascawai@gmail.com>
2025-03-31 22:13:04 -0300

    Include JWT and Journalist routes

--- case-opovo/app/Models/Journalist.php
+++ case-opovo/app/Models/Journalist.php
@@ -2,13 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Journalist extends Model
+class Journalist extends Authenticatable implements JWTSubject
 {
     protected $table = 'journalists';
 
     protected $primaryKey = 'id';
 
-    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
+    protected $fillable = ['first_name', 'last_name', 'email'];
+
+    protected $hidden = ['password'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- case-opovo/routes/api.php
+++ case-opovo/routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTAuthController;
+use App\Http\Middleware\JwtMiddleware;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function (Request $request) {
+    return $request->headers;
+})->withoutMiddleware('auth:api');
+
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::post('me', [JWTAuthController::class, 'getJournalist']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
